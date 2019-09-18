48dd9289bb6529d6a9ce9ce2f9e332b704452e8b
Eric R. Kock <eric.r.kock@gmail.com>
2019-09-18 11:51:32 +0100

    Updates

---
+++ precache-manifest.126d0dd251df0698d8a91fe2a4b4344f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f808b8af89daaeb0c76c96aa8c2574",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6052075ea977b35144e5",
+    "url": "/portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "170f37882959cd70f181",
+    "url": "/portfolio/static/css/main.58eeb5a2.chunk.css"
+  },
+  {
+    "revision": "6052075ea977b35144e5",
+    "url": "/portfolio/static/js/2.ee1ba50b.chunk.js"
+  },
+  {
+    "revision": "170f37882959cd70f181",
+    "url": "/portfolio/static/js/main.fb7bacae.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "0864177f5df19143df07269b69b86951",
+    "url": "/portfolio/static/media/drum-machine.0864177f.jpg"
+  },
+  {
+    "revision": "a86420626bc4c0dc08864768df9ec008",
+    "url": "/portfolio/static/media/front-end-libraries-certificate.a8642062.jpg"
+  },
+  {
+    "revision": "0e79e28e8466678e3c254f05f555be65",
+    "url": "/portfolio/static/media/js-calculator.0e79e28e.jpg"
+  },
+  {
+    "revision": "ccbbe6c4cfb93a437134549fa835b0c6",
+    "url": "/portfolio/static/media/markdown-previewer.ccbbe6c4.jpg"
+  },
+  {
+    "revision": "7d480832ffa4346933549cf6170f527b",
+    "url": "/portfolio/static/media/pomodoro-clock.7d480832.jpg"
+  },
+  {
+    "revision": "3503c1a9b9055f20902963eeb12bcf95",
+    "url": "/portfolio/static/media/random-quote-machine.3503c1a9.jpg"
+  }
+]);
